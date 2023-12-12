a1ac77f8baa5060918fba5f0441aa965b4c016d9
Rcparsln811 <receparslan811@gmail.com>
2023-12-12 20:01:27 +0300

    bugfix

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from 'react-native';
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Hello, React Native!</Text>
+      <Text>Hello, React ffNative!</Text>
     </View>
   );
 };
